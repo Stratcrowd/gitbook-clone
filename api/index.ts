4fe7b6d46f28503967e60d792580f60de9cb3568
digitalvishwain-prog <digitalvishwa.in@gmail.com>
2026-01-13 16:14:15 +0530

    Fix: use path alias to resolve server routes in Vercel build

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "../server/routes.js";
+import { registerRoutes } from "@server/routes";
 import { createServer } from "http";
 
 const app = express();
